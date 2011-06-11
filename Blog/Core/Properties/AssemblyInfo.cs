94f25cd8cebe3011b5d7a13b0100c40600ae21e1
Andreas Nordseth <anordseth@gmail.com>
2011-06-12 01:42:09 +0200

    initial project commit

---
+++ Blog/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("194091ca-fc9a-4148-81f6-1ba7c861f91b")]
+
---
+++ Blog/Web/App/Start/StructuremapMvc.cs
@@ -0,0 +1,16 @@
+using System.Web.Mvc;
+using StructureMap;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(Web.App_Start.StructuremapMvc), "Start")]
+
+namespace Web.App_Start
+{
+    public static class StructuremapMvc
+    {
+        public static void Start()
+        {
+            var container = (IContainer)IoC.Initialize();
+            DependencyResolver.SetResolver(new SmDependencyResolver(container));
+        }
+    }
+}
